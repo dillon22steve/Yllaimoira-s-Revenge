89b856746610cb5775b732e80336f0158813dc8d
Steven Dillon <std42400@uga.edu>
2023-11-07 23:56:05 -0500

    Added more spells and attacks

--- utilz/constants/character_constants/attacks/AttackConstants.java
+++ utilz/constants/character_constants/attacks/AttackConstants.java
@@ -7,12 +7,19 @@ public class AttackConstants {
     public static Attack Kick;
     public static Attack Punch;
     public static Attack Slap;
+    public static Attack ShieldBash;
+    public static Attack Riposte;
+    public static Attack Counter;
 
 
     public static void InitAttacks() {
         Kick = new Attack(5, 1, 1, "Kick");
         Punch = new Attack(5, 1, 1, "Punch");
         Slap = new Attack(3, 0, 1, "Slap");
+
+        ShieldBash = new Attack(10, 1, 1, "Shield Bash");
+        Counter = new Attack(14, 2, 2, "Counter");
+        Riposte = new Attack(18, 3, 3, "Riposte");
     } //InitAttacks
     
 } //AttackConstants
--- utilz/constants/character_constants/attacks/spells/DamageSpells.java
+++ utilz/constants/character_constants/attacks/spells/DamageSpells.java
@@ -7,6 +7,7 @@ public class DamageSpells {
     public static Spell Firebolt;
     public static Spell Frostbite;
     public static Spell Thunderclap;
+    public static Spell TollTheDead;
     public static Spell BurningHands;
     public static Spell ChaosBolt;
     public static Spell EarthTremor;
@@ -19,28 +20,38 @@ public class DamageSpells {
     public static Spell IceStorm;
     public static Spell WallOfFire;
 
+    //Warlock specific
     public static Spell EldritchBlast;
 
+    //Druid
     public static Spell ThornWhip;
     public static Spell Moonbeam;
     public static Spell TidalWave;
 
+    //Bard specific
     public static Spell ViciousMockery;
 
+    //Wizard Specific
     public static Spell PhantasmalKiller;
+
+    //Cleric specific
+    public static Spell SpiritGuardians;
 
 
     public static void InitDamageSpells() {
         Firebolt = new Spell(10, 1, 1, "Firebolt");
         Frostbite = new Spell(10, 1, 1, "Frostbite");
         Thunderclap = new Spell(10, 1, 1, "Thunderclap");
+        TollTheDead = new Spell(10, 1, 1, "Toll the Dead");
+        ThornWhip = new Spell(10, 1, 1, "Thorn Whip");
 
         //Need to update this to account for AOE
         EarthTremor = new Spell(6, 1, 2, "Earth Tremor");
 
         BurningHands = new Spell(18, 3, 3, "Burning Hands");
-        CloudOfDaggers = new Spell(16, 4, 4, "Cloud of Daggers");
+        CloudOfDaggers = new Spell(22, 4, 4, "Cloud of Daggers");
         ChaosBolt = new Spell(22, 3, 5, "Chaos Bolt");
+        Moonbeam = new Spell(22, 2, 5, "Moonbeam");
 
         //Need to update this to account for AOE
         ThunderWave = new Spell(16, 2, 6, "Thunderwave");
@@ -50,6 +61,7 @@ public class DamageSpells {
 
         //Need to update this to account for AOE
         Shatter = new Spell(24, 3, 8, "Shatter");
+        SpiritGuardians = new Spell(24, 3, 8, "Spirit Guardians");
 
         //Need to update this to account for AOE
         Fireball = new Spell(48, 8, 10, "Fireball");
